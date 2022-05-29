c8cf6dbedc34c75b500f1366aad594318616679d
dougiemath <douglasimatheson@gmail.com>
2022-05-29 08:33:55 +0000

    standard privacy policy added

--- home/views.py
+++ home/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 def index (request):
     """A view to return the home page"""
     return render(request, 'home/index.html')
+
+def privacy_policy(request):
+    """A view to return the privacy policy page."""
+    return render(request, 'home/privacy_policy.html')
