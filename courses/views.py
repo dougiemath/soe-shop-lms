5ec42f9b83551f1442992938e5bfd40f86484361
dougiemath <douglasimatheson@gmail.com>
2022-05-30 10:52:08 +0000

    added filtering to base html/views.py

--- courses/views.py
+++ courses/views.py
@@ -1,16 +1,35 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
+from django.db.models import Q
 
 from . models import Course, Category
-# Create your views here.
 
 def courses(request):
     """A view to return page which will display all the courses 
-    that are available."""
+    that are available and filter/sort."""
 
     courses = Course.objects.all()
+    query = None
+    categories = None
+
+    if request.GET:
+
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            courses = courses.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                return redirect(reverse('courses'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            courses = courses.filter(queries)
 
     context = {
         'courses': courses,
+        'search_term': query,
+        'current_categories': categories
     }
 
     return render(request, 'courses/courses.html', context)
