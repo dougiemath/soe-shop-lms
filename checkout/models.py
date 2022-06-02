e36e6a399c6223b391d49bd10b867091c6c730af
dougiemath <douglasimatheson@gmail.com>
2022-06-02 09:14:17 +0000

    Initial checkout forms + signals created

--- checkout/models.py
+++ checkout/models.py
@@ -1,6 +1,7 @@
 import uuid
 from django.db import models
-
+from django.db.models import Sum
+from django.conf import settings
 from django_countries.fields import CountryField
 
 from courses.models import Course
@@ -11,9 +12,9 @@ class Order(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     country = CountryField(blank_label='Country *', null=True, blank=False)
-    postcode = models.CharField(max_length=20, null=True, blank=True)
+    postcode = models.CharField(max_length=20, null=False, blank=True)
     town_or_city = models.CharField(max_length=40, null=True, blank=False)
-    street_address1 = models.CharField(max_length=80, null=True, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
     county = models.CharField(max_length=80, null=True, blank=True)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
