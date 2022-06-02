6eeff751909fdeebb383e27353f4bc9ed2a4a139
dougiemath <douglasimatheson@gmail.com>
2022-06-02 10:21:41 +0000

    further styling done oncheckout page

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import OrderForm
+
+
 def checkout(request):
     """view to render the checkout page"""
-    return render(request, 'checkout/checkout.html')
+    bag = request.session.get('bag',{})
+    if not bag:
+        messages.error(request, "You have nothing in your bag at the moment.")
+        return redirect(reverse('index'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+
+    context = {
+
+        'order_form': order_form
+
+    }
+
+    return render(request, 'checkout/checkout.html', context)
