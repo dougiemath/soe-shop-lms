cce556a0fa773259b6bb5b69ccc43dfb11308d4d
dougiemath <douglasimatheson@gmail.com>
2022-06-13 14:28:04 +0000

    initial edit course feature added

--- lms/urls.py
+++ lms/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
     path('add_lesson/', views.add_lesson, name='add_lesson'),
     path('add_lesson_category/', views.add_lesson_category, name='add_lesson_category'),
     path('add_new_shop_course/', views.add_new_shop_course, name='add_new_shop_course'),
-    path('edit/', views.edit, name='edit'),
+    path('manage/', views.manage, name='manage'),
     path('edit/<int:lesson_id>/', views.edit_lesson, name='edit_lesson'),
+    path('delete/<int:lesson_id>/', views.delete_lesson, name='delete_lesson'),
+    path('edit_shop_content/<int:course_id>', views.edit_shop_content, name='edit_shop_content'),
 ]
--- lms/views.py
+++ lms/views.py
@@ -1,57 +1,3 @@
-# from django.shortcuts import render, get_object_or_404, redirect
-# from django.contrib import messages
-# from django.contrib.auth.decorators import login_required
-
-# from .models import CourseSkill
-# from profiles.models import UserProfile
-# from courses.models import Course
-
-# @login_required
-# def lms(request):
-
-#     courseskills = CourseSkill.objects.all()
-#     user = get_object_or_404(UserProfile, user=request.user)
-#     profile = get_object_or_404(UserProfile, user=request.user)
-#     courses = profile.course_bought.all()
-#     print(courseskills)
-#     print(profile.course_bought)
-#     print(user)
-
-#     if courseskills not in courses:
-#         print(courseskills)
-
-#     context = {
-#         'courseskills': courseskills,
-#     }
-
-#     return render(request, 'lms/lms.html', context)
-
-
-# def lms_content(request, courseskill_id):
-#     """A view to return details for each course/type."""
-
-#     courseskill = get_object_or_404(CourseSkill, uuid=uuid)
-#     print(courseskill)
-#     profile = get_object_or_404(UserProfile, user=request.user)
-#     print(profile)
-#     courses = profile.course_bought.all()
-#     print(courses)
-#     # current_course = get_object_or_404(Course, courseskill=courseskill)
-#     # print(current_course)
-#     # if current_course not in courses:
-        
-#     #     messages.error(request, 'You cannot access this page without buying the course.')
-#     #     return redirect('lms')
-
-    
-#     context = {
-#         'courseskill': courseskill,
-#     }
-
-#     return render(request, 'lms/lms_content.html', context)
-
-
-
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
@@ -63,30 +9,25 @@ from courses.models import Course
 
 @login_required
 def lms(request):
-
+    # gets all lessons
     lessons = Lessons.objects.all()
+    # gets current logged in user
     user = get_object_or_404(UserProfile, user=request.user)
+    print("...............")
+    print(user)
+    print("...............")
+    # gets profile details of curent logged in user
     profile = UserProfile.objects.get(user=request.user)
+    print("...............")
+    print(profile)
+    print("...............")
     course_categories = Lessons.objects.filter(category=1)
     courses_bought = profile.course_bought.all()
     lesson_category = LessonCategory.objects.all()
-    print("---------------")
-    # print("LESSONS: ", lessons)
-    # print("---------------")
-    # print("COURSE CATEGORIES: ", course_categories)
-    # print("---------------")
-    # print("CATEGORIES: ", lesson_category)
-    # print("---------------")
-    # print(user)
-    print(Lessons)
-
-    # for lesson in lessons:
-    #     for course in courses_bought:
-    #         if course.course_category == lesson.category:
-    #             print(course)
-
-    if lessons not in courses_bought:
-        print(lessons)
+    if request.user.is_authenticated:
+        print("user is authenticated")
+    else:
+        print("something went wrong")
 
     context = {
         'lessons': lessons,
@@ -97,7 +38,7 @@ def lms(request):
 
     return render(request, 'lms/lms.html', context)
 
-
+@login_required
 def lms_content(request, lesson_id):
     """A view to return details for each course/type."""
     print("Content")
@@ -141,7 +82,7 @@ def lms_content(request, lesson_id):
 
     return render(request, 'lms/lms_content.html', context)
 
-
+@login_required
 def add(request):
     """returns a contents-style page for 
     adding new course/shop content"""
@@ -149,7 +90,7 @@ def add(request):
 
 
 # .......................................
-
+@login_required
 def add_lesson(request):
     """ Add a lesson to the LMS """
     if request.method == "POST":
@@ -175,7 +116,7 @@ def add_lesson(request):
 
 
     # .......................................
-
+@login_required
 def add_lesson_category(request):
     """ Add a lesson category to the LMS """
     if request.method == "POST":
@@ -201,7 +142,7 @@ def add_lesson_category(request):
 
 
 # .......................................
-
+@login_required
 def add_new_shop_course(request):
     """ Add a lesson category to the LMS """
     if request.method == "POST":
@@ -225,35 +166,38 @@ def add_new_shop_course(request):
 
     return render(request, template, context)
 
-
-def edit(request):
+@login_required
+def manage(request):
     """returns a contents-style page for 
     editing course/shop content"""
     lessons = Lessons.objects.all().order_by('category')
     lesson_category = LessonCategory.objects.all()
+    courses = Course.objects.all()
 
     context = {
         'lessons': lessons,
         'lesson_category': lesson_category,
+        'courses': courses,
 
     }
 
-    return render(request, 'lms/edit.html', context)
+    return render(request, 'lms/manage.html', context)
 
+@login_required
 def edit_lesson(request, lesson_id):
     
     lesson = get_object_or_404(Lessons, pk=lesson_id)
     
     if request.method == 'POST':
-        form = LessonCategoryForm(request.POST, request.FILES, instance=lesson)
+        form = LessonForm(request.POST, request.FILES, instance=lesson)
         if form.is_valid():
             form.save()
             messages.success(request, 'Success!')
-            return redirect(reverse('edit'))
+            return redirect(reverse('manage'))
         else:
             messages.error(request, 'Failed.')
     else:
-        form = LessonCategoryForm(instance=lesson)
+        form = LessonForm(instance=lesson)
         messages.info(request, f'You are editing {lesson.name}')
 
     template = 'lms/edit_lesson.html'
@@ -264,3 +208,55 @@ def edit_lesson(request, lesson_id):
     }
 
     return render(request, template, context)
+
+@login_required
+def delete_lesson(request, lesson_id):
+
+    lesson = get_object_or_404(Lessons, pk=lesson_id)
+    lesson.delete()
+    messages.success(request, 'Lesson Deleted')
+    return redirect(reverse('manage'))
+
+
+
+
+
+
+
+
+
+
+
+
+@login_required
+def edit_shop_content(request, course_id):
+    course = get_object_or_404(Course, pk=course_id)
+    
+    if request.method == 'POST':
+        form = NewShopCourseForm(request.POST, request.FILES, instance=lesson)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Success!')
+            return redirect(reverse('manage'))
+        else:
+            messages.error(request, 'Failed.')
+    else:
+        form = NewShopCourseForm(instance=course)
+        messages.info(request, f'You are editing {course.name}')
+
+    template = 'lms/edit_shop_content.html'
+
+    context = {
+        'form': form,
+        'course': course,
+    }
+
+    return render(request, template, context)
+
+# @login_required
+# def edit_shop_content(request, lesson_id):
+
+#     lesson = get_object_or_404(Lessons, pk=lesson_id)
+#     lesson.delete()
+#     messages.success(request, 'Lesson Deleted')
+#     return redirect(reverse('manage'))
