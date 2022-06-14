16bd7a655da20a9cc974b7f1ca9ae1daeb7b69f0
dougiemath <douglasimatheson@gmail.com>
2022-06-14 14:00:47 +0000

    added user content to LMS

--- lms/admin.py
+++ lms/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from django_summernote.admin import SummernoteModelAdmin
 from  embed_video.admin  import  AdminVideoMixin
 
 from .models import Lessons, LessonCategory
@@ -7,10 +6,9 @@ from .models import Lessons, LessonCategory
 
 @admin.register(Lessons)
 class LessonAdmin(admin.ModelAdmin):
-    list_display = ('id', 'category','name', 'exam_section', 'question_type', 'video', 'upload_questions')
-    # summernote_fields = ('exam_description', 'question_overview', 'sample_question_text', 'sample_question_questions', 'question_approach', 'further_study',)
+    list_display = ('category','name', 'exam_section', 'question_type', 'video', 'upload_questions')
 
 class LessonCategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
+    list_display = ('name',)
 
 admin.site.register(LessonCategory, LessonCategoryAdmin)
