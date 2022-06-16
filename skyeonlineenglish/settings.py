66b0d71a02526aafcb3787b5fed70e2cb04ad207
dougiemath <douglasimatheson@gmail.com>
2022-06-16 08:31:18 +0000

    Push for live test

--- skyeonlineenglish/settings.py
+++ skyeonlineenglish/settings.py
@@ -177,8 +177,6 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
-SUMMERNOTE_THEME = 'bs4'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
