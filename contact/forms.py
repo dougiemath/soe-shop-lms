9ac5ae6efb4fa44ba6057ea6572dfc88069afe36
dougiemath <douglasimatheson@gmail.com>
2022-06-16 16:15:32 +0000

    contact form added as model

--- contact/forms.py
+++ contact/forms.py
@@ -1,3 +1,8 @@
-from django import forms
+from django.forms import ModelForm
+from .models import Contact
 
-# Create your forms here.
+
+class ContactForm(ModelForm):
+    class Meta:
+        model = Contact
+        fields = '__all__'
--- contact/views.py
+++ contact/views.py
@@ -1,25 +1,52 @@
-from django.conf import settings
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404, reverse
 from django.contrib import messages
-from django.core.mail import send_mail
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .forms import ContactForm
+from.models import Contact
 
 def contact(request):
-    if request.method == "POST":
-        message_name = request.POST['message-name']
-        message_email = request.POST['message-email']
-        message = request.POST['message']
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'Thanks for your email, we will contact you shortly.')
+            return redirect('index')
+    form = ContactForm()
+    context = {'form': form}
+    return render(request, 'contact/contact.html', context)
 
-        send_mail(
-            message_name, 
-            message,  
-            message_email,  
-            [settings.EMAIL_HOST_USER]
-        )
 
-        messages.success(request, f'Thank you {message_name}, we will contact you shortly.')
-        return render(request, "contact/contact.html")
+def contact_inbox(request):
+    emails = Contact.objects.all()
+    print(emails)
+
+    context = {
+        'emails': emails,
+    }
+
+    return render(request, 'contact/contact_inbox.html', context)
+
+def contact_received_email(request, contact_id):
+    """A view to return details for each course/type."""
+
+    email = get_object_or_404(Contact, pk=contact_id)
+
+    context = {
+        'email': email,
+    }
+
+    return render(request, 'contact/contact_received_email.html', context)
+
+
+@login_required
+def delete_email(request, contact_id):
+    """allows the user to delete a product in the shop"""
+    if request.user.is_superuser:
+        email = get_object_or_404(Contact, pk=contact_id)
+        email.delete()
+        messages.success(request, 'Email deleted')
+        return redirect(reverse('contact_inbox'))
     else:
-        return render(request, "contact/contact.html")
-
+        messages.error(request, 'You do not have access to this page.')
+        return redirect(reverse('index'))
