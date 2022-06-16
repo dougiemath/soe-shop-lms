41f7e648539c444720e088cfabfdd532bb6446ee
dougiemath <douglasimatheson@gmail.com>
2022-06-16 13:53:15 +0000

    reworked contact form

--- contact/views.py
+++ contact/views.py
@@ -1,33 +1,25 @@
-from django.shortcuts import render, redirect, reverse
-from django.core.mail import send_mail, BadHeaderError
-from django.http import HttpResponse
+from django.conf import settings
+from django.shortcuts import render
 from django.contrib import messages
-
-
-from .forms import ContactForm
+from django.core.mail import send_mail
 
 # Create your views here.
 
 def contact(request):
-    if request.method == 'POST':
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            subject = "Website Inquiry"
-            body = {
-                'first_name': form.cleaned_data['first_name'],
-                'last_name': form.cleaned_data['last_name'],
-                'email': form.cleaned_data['email_address'], 
-                'message': form.cleaned_data['message'],
-                }
-            message = "\n".join(body.values())
-            
-            try:
-                send_mail(subject, message, 'website@inquiry.com', ['website@inquiry.com'])
-                messages.success(request, f'Your email has been sent.  We will contact you shortly.')
-            except BadHeaderError:
-                return HttpResponse('Invalid header found.')
-                
-            return redirect(reverse('index'))
+    if request.method == "POST":
+        message_name = request.POST['message-name']
+        message_email = request.POST['message-email']
+        message = request.POST['message']
 
-    form = ContactForm()
-    return render(request, "contact/contact.html", {'form': form})
+        send_mail(
+            message_name, 
+            message,  
+            message_email,  
+            [settings.EMAIL_HOST_USER]
+        )
+
+        messages.success(request, f'Thank you {message_name}, we will contact you shortly.')
+        return render(request, "contact/contact.html")
+    else:
+        return render(request, "contact/contact.html")
+
