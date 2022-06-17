cff5eed137d87776c64d8728b5442a50e698d375
dougiemath <douglasimatheson@gmail.com>
2022-06-17 08:04:41 +0000

    contact access added, push for live test

--- contact/views.py
+++ contact/views.py
@@ -16,28 +16,37 @@ def contact(request):
     context = {'form': form}
     return render(request, 'contact/contact.html', context)
 
-
+@login_required
 def contact_inbox(request):
-    emails = Contact.objects.all()
-    print(emails)
+    """A view to create an inbox style page for emails"""
+    if request.user.is_superuser:
+        emails = Contact.objects.all()
+        print(emails)
 
-    context = {
-        'emails': emails,
-    }
+        context = {
+            'emails': emails,
+        }
 
-    return render(request, 'contact/contact_inbox.html', context)
+        return render(request, 'contact/contact_inbox.html', context)
+    else:
+        messages.error(request, 'You do not have access to this page.')
+        return redirect(reverse('index'))
 
+@login_required
 def contact_received_email(request, contact_id):
-    """A view to return details for each course/type."""
+    """A view to give access to full email."""
+    if request.user.is_superuser:
+        email = get_object_or_404(Contact, pk=contact_id)
 
-    email = get_object_or_404(Contact, pk=contact_id)
-
-    context = {
-        'email': email,
-    }
-
-    return render(request, 'contact/contact_received_email.html', context)
-
+        context = {
+            'email': email,
+        }
+        
+        return render(request, 'contact/contact_received_email.html', context)
+    else:
+        messages.error(request, 'You do not have access to this page.')
+        return redirect(reverse('index'))
+        
 
 @login_required
 def delete_email(request, contact_id):
